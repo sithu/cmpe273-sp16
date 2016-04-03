b812f1429b825b2ead245bc6201ca9e83b212b76
saung <sithu_aung@intuit.com>
2016-04-03 16:31:45 -0700

    update comment

--- mid-term/sample.go
+++ mid-term/sample.go
@@ -5,6 +5,8 @@ package main
 // (when encountering in-order) or nil if k isn't present.
 // The time complexity is O(h) where h is the tree height.
 // The O(1) additional space is needed.
+
+// #### Answer #####
 func FindFirstK(tree *BSTree, k int) *BSTree {
 	var c *BSTree
 	node := tree
